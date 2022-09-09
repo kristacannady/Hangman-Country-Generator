d11bcd3376529340b4f1a681c2e9d081ee79aae9
Krista Cannady <kristalcannady@gmail.com>
2022-09-09 14:26:07 -0400

    working keyboard

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,21 @@
 var countries = [
-    "Egypt",
-    "Bulgaria",
-    "France",
-    "Nepal",
-    "England",
-    "Spain",
-    "Afghanistan",
-    "Albania",
-    "Indonesia",
-    "Lebanon",
-    "Lithuania",
-    "Singapore",
-    "Uganda",
-    "Mozambique",
-    "Vietnam",
-    "Jamaica",
-    "Croatia"
+    "egypt",
+    "bulgaria",
+    "france",
+    "nepal",
+    "england",
+    "spain",
+    "afghanistan",
+    "albania",
+    "indonesia",
+    "lebanon",
+    "lithuania",
+    "singapore",
+    "uganda",
+    "mozambique",
+    "vietnam",
+    "jamaica",
+    "croatia"
 ]
 
 var answer = "";
@@ -32,7 +32,7 @@ function keyboard() {
     var buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
         `<button
             class="btn"
-            id='` + letter + `
+            id='` + letter + `'
             onClick="handleGuess('` + letter + `')"
         >
             ` + letter + `
@@ -40,7 +40,39 @@ function keyboard() {
         `).join('');
 
         document.getElementById('keyboard').innerHTML = buttonsHTML;
-};
+}
+
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedCountry();
+        checkIfGameWon();
+} else if (answer.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
+}
+}
+
+function updateHangmanPicture() {
+    document.getElementById('hangmanPic').src = '.Assets/images/' + mistakes + '.png';
+}
+
+function checkIfGameWon() {
+    if (countryStatus === answer) {
+        document.getElementById('keyboard').innerHTML = "You Won!!!";
+    }
+}
+
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById('countrySpotlight').innerHTML = 'The answer was: ' + answer;
+        document.getElementById('keyboard').innerHTML = "You Lost!!!!";
+    }
+}
 
 function guessedCountry() {
     countryStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
@@ -48,10 +80,24 @@ function guessedCountry() {
     document.getElementById('countrySpotlight').innerHTML = countryStatus;
 }
 
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangmanPic').src = './images/0.png';
+
+randomWord();
+guessedWord();
+updateMistakes();
+generateButtons();
+}
+
 // this line was creating problems with the keyboard and I wasn't
 // sure if we would want to include the "wrong guesses" tracker anyway
 // document.getElementById().innerHTML('maxWrong') = maxWrong;
-
 
 randomCountry();
 keyboard();
